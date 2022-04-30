72cd884755a8da4839835c529ecbc4ca9da680e7
Bulat Khsamov <skekwait@bk.ru>
2022-04-30 19:32:52 +0300

    update

---
+++ localhost/frontend/js/requests.js
@@ -0,0 +1,9 @@
+
+
+fetch("https://google.com").then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    console.log(data);
+  }).catch(function() {
+    console.log("Booo");
+  });
