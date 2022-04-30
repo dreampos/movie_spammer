1c90e8318bf39bcc148a61a03f33003966c29844
Bulat Khsamov <skekwait@bk.ru>
2022-04-30 19:54:34 +0300

    update

--- localhost/frontend/js/requests.js
+++ localhost/frontend/js/requests.js
@@ -1,9 +1,21 @@
 
 
-fetch("https://google.com").then(function(response) {
-    return response.json();
-  }).then(function(data) {
-    console.log(data);
-  }).catch(function() {
-    console.log("Booo");
-  });
+// fetch("https://google.com").then(function(response) {
+//     return response.json();
+//   }).then(function(data) {
+//     console.log(data);
+//   }).catch(function() {
+//     console.log("Booo");
+//   });
+
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+
+httpGet("https://stackoverflow.com/questions/247483/http-get-request-in-javascript");
